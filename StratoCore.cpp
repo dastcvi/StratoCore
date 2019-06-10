91b2035a4fb99d569067ab91106b60f1a12a6a6e
dastcvi <david.stclair@colorado.edu>
2019-06-10 10:24:52 -0600

    Created StratoCore template

---
+++ StratoCore.cpp
@@ -0,0 +1,10 @@
+/*
+ *  StratoCore.cpp
+ *  Author:  Alex St. Clair
+ *  Created: June 2019
+ *  
+ *  This file implements an Arduino library (C++ class) with core functionality
+ *  for LASP Strateole payload interface boards
+ */
+
+#include "StratoCore.h"
---
+++ StratoCore.h
@@ -0,0 +1,24 @@
+/*
+ *  StratoCore.h
+ *  Author:  Alex St. Clair
+ *  Created: June 2019
+ *  
+ *  This file declares an Arduino library (C++ class) with core functionality
+ *  for LASP Strateole payload interface boards
+ */
+
+#ifndef STRATOCORE_H
+#define STRATOCORE_H
+
+#include "Arduino.h"
+#include "HardwareSerial.h"
+#include "WProgram.h"
+#include <stdint.h>
+
+class StratoCore {
+public:
+protected: // available to StratoCore and child classes
+private: // available only to StratoCore
+};
+
+#endif /* STRATOCORE_H */
