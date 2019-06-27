8adfe366886d958d1a36348bf70b4095175bc519
dastcvi <david.stclair@colorado.edu>
2019-06-27 09:51:14 -0600

    Implemented custom sorted list for scheduling

--- StratoScheduler.cpp
+++ StratoScheduler.cpp
@@ -10,15 +10,20 @@
 #include "StratoScheduler.h"
 #include "StratoGroundPort.h"
 
+StratoScheduler::StratoScheduler()
+{
+    schedule_size = 0;
+}
+
 uint8_t StratoScheduler::CheckSchedule()
 {
     uint8_t action = NO_SCHEDULED_ACTION;
 
     // if it's time for the top action, set it and remove it from the queue
-    if (schedule_queue.size() > 0 && schedule_queue.top().time <= now()) {
-        action = schedule_queue.top().action;
-        schedule_queue.pop();
-    }
+    // if (schedule_queue.size() > 0 && schedule_queue.top().time <= now()) {
+    //     action = schedule_queue.top().action;
+    //     schedule_queue.pop();
+    // }
 
     return action;
 }
@@ -26,32 +31,103 @@ uint8_t StratoScheduler::CheckSchedule()
 bool StratoScheduler::AddAction(uint8_t action, time_t seconds_from_now)
 {
     // enforce a software size limit to avoid accidentally filling memory
-    if (schedule_queue.size() > MAX_SCHEDULE_SIZE) {
+    if (schedule_size >= MAX_SCHEDULE_SIZE) {
         log_error("Schedule queue full");
         return false;
     }
 
     // calculate the time_t value given the current time, place on the queue
-    schedule_queue.emplace(action, now() + seconds_from_now);
+    SchedulePush(action, now() + seconds_from_now);
     return true;
 }
 
 bool StratoScheduler::AddAction(uint8_t action, TimeElements exact_time)
 {
     // enforce a software size limit to avoid accidentally filling memory
-    if (schedule_queue.size() > MAX_SCHEDULE_SIZE) {
+    if (schedule_size >= MAX_SCHEDULE_SIZE) {
         log_error("Schedule queue full");
         return false;
     }
 
     // place on the queue
-    schedule_queue.emplace(action, makeTime(exact_time));
+    SchedulePush(action, makeTime(exact_time));
     return true;
 }
 
 void StratoScheduler::ClearSchedule()
 {
-    while (schedule_queue.size() > 0) {
-        schedule_queue.pop();
+    while (schedule_size > 0) {
+        Serial.println(schedule_size);
+        SchedulePop();
     }
+}
+
+// ------- schedule queue functions -------
+
+bool StratoScheduler::SchedulePush(uint8_t action, time_t schedule_time)
+{
+    if (schedule_size >= MAX_SCHEDULE_SIZE) return false;
+
+    // create the new action
+    ScheduleItem_t * new_item = new ScheduleItem_t(action, schedule_time, NULL, NULL);
+
+    // check that it's good
+    if (new_item == NULL) return false;
+
+    schedule_size++;
+
+    // if empty, create in place
+    if (schedule_top == NULL) {
+        schedule_top = new_item;
+        return true;
+    }
+
+    // create an iterator for moving through the queue, starting with the first element
+    ScheduleItem_t * cur = schedule_top;
+
+    // move through the queue until either the end is reach, or the insertion spot is found
+    while (new_item->time > cur->time && cur->next != NULL) {
+        cur = cur->next;
+    }
+
+    // place
+    if (new_item->time <= cur->time) {
+        if (cur->prev == NULL) {
+            schedule_top = new_item;
+            cur->prev = new_item;
+            new_item->next = cur;
+        } else {
+            new_item->prev = cur->prev;
+            cur->prev->next = new_item;
+            cur->prev = new_item;
+            new_item->next = cur;
+        }
+    } else {
+        cur->next = new_item;
+        new_item->prev = cur;
+    }
+
+    return true;
+}
+
+ScheduleItem_t * StratoScheduler::SchedulePeek()
+{
+    if (schedule_size == 0) {
+        return NULL;
+    } else {
+        return schedule_top;
+    }
+}
+
+void StratoScheduler::SchedulePop()
+{
+    if (schedule_size == 0) return;
+
+    ScheduleItem_t * tmp = schedule_top;
+
+    schedule_top = tmp->next;
+
+    delete tmp;
+
+    schedule_size--;
 }
--- StratoScheduler.h
+++ StratoScheduler.h
@@ -12,19 +12,26 @@
 
 #include <TimeLib.h>
 #include <stdint.h>
-#include <queue>
-#include <vector>
-#include <functional>
 
 #define NO_SCHEDULED_ACTION 0
-#define MAX_SCHEDULE_SIZE   32
+#define MAX_SCHEDULE_SIZE   ((uint8_t) 32) // must be 0-255
 
-using namespace std;
+// define a struct for use only as a container for scheduled actions
+struct ScheduleItem_t {
+    // constructor
+    ScheduleItem_t(uint8_t act, time_t t, ScheduleItem_t * p, ScheduleItem_t * n) : action(act), time(t), prev(p), next(n) { }
+
+    // data
+    uint8_t action;
+    time_t time;
+    ScheduleItem_t * prev;
+    ScheduleItem_t * next;
+};
 
 class StratoScheduler {
 public:
     // empty constructors and destructors
-    StratoScheduler() { };
+    StratoScheduler();
     ~StratoScheduler() { };
 
     // returns 0 if no action ready, or the id if one is ready
@@ -36,29 +43,89 @@ public:
 
     // todo: consider adding adjustment function for when GPS time update is greater than xx seconds
     // in order to ensure that the schedule isn't messed up when time changes underneath it
+    // void UpdateScheduleTime();
 
     // called after every mode switch
     void ClearSchedule();
 
-private:
-    // define a private struct for use only as a container for scheduled actions
-    struct ScheduleItem_t {
-        uint8_t action;
-        time_t time;
-        
-        // constructor to allow use of priority_queue emplace function
-        ScheduleItem_t(uint8_t a, time_t t) : action(a), time(t) { }
-    };
+//private:
 
-    // this private struct serves only to provide the operator that allows the priority queue to sort actions
-    struct CompareItems { 
-        bool operator()(ScheduleItem_t const& i1, ScheduleItem_t const& i2) {
-            return i1.time > i2.time; 
-        }
-    }; 
+    bool SchedulePush(uint8_t action, time_t schedule_time); // add to schedule
+    ScheduleItem_t * SchedulePeek(); // look at first item without removing
+    void SchedulePop(); // remove (and delete!) the first item
 
-    // define a priority queue for sorting and keeping all of the scheduled items
-    priority_queue<ScheduleItem_t, vector<ScheduleItem_t>, CompareItems> schedule_queue;
+    uint8_t schedule_size; // num items in schedule
+    ScheduleItem_t * schedule_top; // pointer to first element
 };
 
 #endif
+
+//   -- test code -- 
+ 
+//   delay(1000);
+//   ScheduleItem_t * tmp;
+  
+//   Serial.println("beginning");
+//   Serial.println(strato.scheduler.schedule_size);
+//   strato.scheduler.SchedulePop();
+//   Serial.println(strato.scheduler.schedule_size);
+//   strato.scheduler.SchedulePush(0, 10);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePush(0, 11);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePush(0, 9);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   strato.scheduler.SchedulePush(0, 12);
+//   strato.scheduler.SchedulePush(0, 12);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePush(0, 8);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePush(0, 13);
+//   strato.scheduler.SchedulePush(0, 13);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePush(0, 7);
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   tmp = strato.scheduler.SchedulePeek();
+//   Serial.println(tmp->time);
+//   strato.scheduler.SchedulePop();
+//   Serial.println(strato.scheduler.schedule_size);
+//   for (int i = 0; i < 35; i++) {
+//     if (i == 4) {
+//       strato.scheduler.SchedulePush(0,1);
+//       continue;
+//     } else if (i == 6) {
+//       strato.scheduler.SchedulePush(0,3);
+//       continue;
+//     }
+//     if (!strato.scheduler.SchedulePush(0,2)) Serial.println(i);
+//   }
+//   strato.scheduler.ClearSchedule();
+//   Serial.println(strato.scheduler.schedule_size);
+//   // -- 
